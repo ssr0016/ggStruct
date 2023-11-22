4002199bb04d598505aa78a738ea3047f926e79d
ssr0016 <samsonrecluta0016@gmail.com>
2023-11-22 11:14:02 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type OptFunc func(*Opts)
+
+type Opts struct {
+	maxConn int
+	id      string
+	tls     bool
+}
+
+func defaultOpts() Opts {
+	return Opts{
+		maxConn: 10,
+		id:      "default",
+		tls:     false,
+	}
+}
+
+func withID(id string) OptFunc {
+	return func(opts *Opts) {
+		opts.id = id
+	}
+}
+
+func withTLS(opts *Opts) {
+	opts.tls = true
+
+}
+
+func withMaxConn(n int) OptFunc {
+	return func(opts *Opts) {
+		opts.maxConn = n
+	}
+}
+
+type Server struct {
+	Opts
+}
+
+func newServer(opts ...OptFunc) *Server {
+	o := defaultOpts()
+	for _, fn := range opts {
+		fn(&o)
+	}
+
+	return &Server{
+		Opts: o,
+	}
+}
+
+func main() {
+	s := newServer(withTLS, withMaxConn(99), withID("bar foo baz"))
+	fmt.Printf("%+v\n", s)
+}
